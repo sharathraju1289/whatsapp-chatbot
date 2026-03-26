ed50aae9d4360691f745530e6173166ecd3b4949
Sharath Kumar Raju Mudduluri <msharathkumarraju@gmail.com>
2026-03-26 13:18:42 +0530

    Create  Metadata.java

---
+++ Metadata.java
@@ -0,0 +1,12 @@
+package com.example.whatsappchatbot.model;
+
+public class Metadata {
+    private String display_phone_number;
+    private String phone_number_id;
+    
+    public String getDisplayPhoneNumber() { return display_phone_number; }
+    public void setDisplayPhoneNumber(String display_phone_number) { this.display_phone_number = display_phone_number; }
+    
+    public String getPhoneNumberId() { return phone_number_id; }
+    public void setPhoneNumberId(String phone_number_id) { this.phone_number_id = phone_number_id; }
+}

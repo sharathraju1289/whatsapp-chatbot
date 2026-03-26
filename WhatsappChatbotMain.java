b8d73ebfe43d15fd25ddd6486ac2512b9ba64bcf
Sharath Kumar Raju Mudduluri <msharathkumarraju@gmail.com>
2026-03-26 18:44:41 +0530

    Create WhatsappChatbotMain.java

---
+++ WhatsappChatbotMain.java
@@ -0,0 +1,12 @@
+package com.example.whatsappchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappChatbotMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsappChatbotMain.class, args);
+    }
+}

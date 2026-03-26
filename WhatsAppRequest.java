59f4b51147e30c3b1c6668bfd8366674e2877f8f
Sharath Kumar Raju Mudduluri <msharathkumarraju@gmail.com>
2026-03-26 12:47:44 +0530

    Create WhatsAppRequest.java

---
+++ WhatsAppRequest.java
@@ -0,0 +1,9 @@
+package com.example.whatsappchatbot.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class WhatsAppRequest {
+    private Object entry;
+}
